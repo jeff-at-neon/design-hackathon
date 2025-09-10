8431dc81190a3dddf319d5340c02d247eba2fa19
Kyle Gilbreath <kyle.gilbreath+data@databricks.com>
2025-09-09 17:00:10 -0700

    Add React frontend with Databricks design system components

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Playground from './Playground';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Main Content - Always show Playground as homepage */}
+      <Playground />
+    </div>
+  );
+}
+
+export default App;
